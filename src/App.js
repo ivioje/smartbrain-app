b57ae687a8f2a0706d9e5c7c7aeb260392cd9b0c
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-10-12 12:31:03 +0100

    added react-tsparticles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
+import React, { useState, useCallback } from 'react';
 import 'tachyons';
-import Particles from 'react-tsparticles';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
-import { useState } from 'react';
 import FaceRecognition from './components/faceRecognition/FaceRecognition';
 
 const App = () => {
   const [input, setIput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [box, setBox] = useState({})
+
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
 
   const USER_ID = 'ivioje';
   const PAT = '8bd5769ed122489188ea547e12097a67';
@@ -46,17 +56,17 @@ const App = () => {
   };
 
   const calculateFaceLocation = (data) => {
-   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-   const image =  document.getElementById('inputImage');
-   const width = Number(image.width);
-   const height = Number(image.height);
-   console.log(width, height);
-   return {
-    leftCol: clarifaiFace.left_col * width,
-    topRow: clarifaiFace.top_row * height,
-    rightCol: width - (clarifaiFace.right_col * width),
-    bottomRow: height - (clarifaiFace.bottom_row * height)
-   }
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
 
   const displayFaceBox = (box) => {
@@ -71,31 +81,136 @@ const App = () => {
   const onButtonSubmit = () => {
     setImageUrl(input)
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-          let data = JSON.parse(result);
-          displayFaceBox(calculateFaceLocation(data))
-          //console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
-        })
-        .catch(error => console.log('error', error));
-
+      .then(response => response.text())
+      .then(result => {
+        let data = JSON.parse(result);
+        displayFaceBox(calculateFaceLocation(data))
+        //console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
+      })
+      .catch(error => console.log('error', error));
   }
 
-  const particleOptions = {
-    particles: {
-      number: {
-        value: 30,
-        density: {
-          enable: true,
-          value_area: 800
+  const options = {
+    "fullScreen": {
+      "enable": true,
+      "zIndex": -1
+    },
+    "particles": {
+      "number": {
+        "value": 10,
+        "density": {
+          "enable": false,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#fff"
+      },
+      "shape": {
+        "type": "dot",
+        "options": {
+          "sides": 9
+        }
+      },
+      "opacity": {
+        "value": 0.2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 50,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "rotate": {
+        "value": 0,
+        "random": true,
+        "direction": "clockwise",
+        "animation": {
+          "enable": true,
+          "speed": 5,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 600,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
         }
       }
-    }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": ["grab"]
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": .7
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 100,
+          "duration": 2,
+          "opacity": .6,
+          "speed": 2
+        },
+        "repulse": {
+          "distance": 50
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
   }
 
   return (
     <div className='App pa2'>
-      <Particles className='particles' params={particleOptions} />
+      <Particles
+        params={options}
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <Navigation />
       <Logo />
       <Rank />
