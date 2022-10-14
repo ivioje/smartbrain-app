19da8583d15832683d99262806f5e8560cf9f40f
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-10-14 12:49:37 +0100

    moved particles to a separate component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import 'tachyons';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
 import FaceRecognition from './components/faceRecognition/FaceRecognition';
+import SignIn from './components/sign-in/SignIn';
+import WebParticles from './components/particles/Particles';
 
 const App = () => {
   const [input, setIput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  const [box, setBox] = useState({})
-
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState('signin')
 
   const USER_ID = 'ivioje';
   const PAT = '8bd5769ed122489188ea547e12097a67';
@@ -90,135 +82,22 @@ const App = () => {
       .catch(error => console.log('error', error));
   }
 
-  const options = {
-    "fullScreen": {
-      "enable": true,
-      "zIndex": -1
-    },
-    "particles": {
-      "number": {
-        "value": 10,
-        "density": {
-          "enable": false,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#fff"
-      },
-      "shape": {
-        "type": "dot",
-        "options": {
-          "sides": 9
-        }
-      },
-      "opacity": {
-        "value": 0.2,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 50,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "rotate": {
-        "value": 0,
-        "random": true,
-        "direction": "clockwise",
-        "animation": {
-          "enable": true,
-          "speed": 5,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 600,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 2
-      },
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": ["grab"]
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "bubble"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 200,
-          "line_linked": {
-            "opacity": .7
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 100,
-          "duration": 2,
-          "opacity": .6,
-          "speed": 2
-        },
-        "repulse": {
-          "distance": 50
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-  }
-
   return (
     <div className='App pa2'>
-      <Particles
-        params={options}
-        id='tsparticles'
-        init={particlesInit}
-        loaded={particlesLoaded}
-      />
+      <WebParticles />
       <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onButtonSubmit={onButtonSubmit}
-      />
-      <FaceRecognition imageUrl={imageUrl} box={box} />
+      {route === 'signin' ?
+        <SignIn /> :
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition imageUrl={imageUrl} box={box} />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/components/particles/Particles.js
@@ -0,0 +1,141 @@
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const WebParticles = () => {
+
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    const options = {
+        "fullScreen": {
+            "enable": true,
+            "zIndex": -1
+        },
+        "particles": {
+            "number": {
+                "value": 10,
+                "density": {
+                    "enable": false,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#fff"
+            },
+            "shape": {
+                "type": "dot",
+                "options": {
+                    "sides": 9
+                }
+            },
+            "opacity": {
+                "value": 0.2,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 2,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 50,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "rotate": {
+                "value": 0,
+                "random": true,
+                "direction": "clockwise",
+                "animation": {
+                    "enable": true,
+                    "speed": 5,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 600,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 2
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": ["grab"]
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "bubble"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 200,
+                    "line_linked": {
+                        "opacity": .7
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 100,
+                    "duration": 2,
+                    "opacity": .6,
+                    "speed": 2
+                },
+                "repulse": {
+                    "distance": 50
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+    }
+
+    return (
+        <div>
+            <Particles
+                params={options}
+                id='tsparticles'
+                init={particlesInit}
+                loaded={particlesLoaded}
+            />
+        </div>
+    )
+}
+
+export default WebParticles
